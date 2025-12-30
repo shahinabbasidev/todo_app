1586aa10506d069ddb3ec0459b95b75c1dc7ab80
shahinabbasidev <shahin.abbasi.dev@gmail.com>
2025-12-30 17:46:17 +0330

    write the CRUD in routes.py and schemas

--- core/tasks/routes.py
+++ core/tasks/routes.py
@@ -1,13 +1,54 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Path,Depends,HTTPException,Query
+from fastapi.responses import JSONResponse
+from tasks.models import TaskModel
+from tasks.schemas import TaskResponseSchema,TaskCreateSchema,TaskUpdateSchema
+from sqlalchemy.orm import Session
+from core.database import get_db
+from typing import List
 
 router = APIRouter(tags=["tasks"],prefix="/todo")
 
-@router.get("/tasks")
-async def retrieve_tasks_list():
+@router.get("/tasks",response_model=List[TaskResponseSchema])
+async def retrieve_tasks_list(db:Session = Depends(get_db)):
 
-    return []
+    result = db.query(TaskModel).all()
+    return result
 
-@router.get("/tasks/{task_id}")
-async def retrieve_tasks_detail():
+@router.get("/tasks/{task_id}",response_model=TaskResponseSchema)
+async def retrieve_tasks_detail(task_id: int = Path(...,gt=0),db:Session = Depends(get_db)):
 
-    return []
+    task_obj = db.query(TaskModel).filter_by(id=task_id).first()
+    if not task_obj:
+        raise HTTPException(status_code=404,detail="task not found")
+    return task_obj
+
+
+@router.post("/tasks",response_model=TaskResponseSchema)
+async def post_tasks(db:Session = Depends(get_db),request = TaskCreateSchema):
+    task_obj = TaskModel(**request.model_dump())
+    db.add(task_obj)
+    db.commit()
+    db.refresh(task_obj)
+    return task_obj
+
+
+@router.put("/tasks/{task_id}",response_model=TaskResponseSchema)
+async def put_tasks(task_id: int = Path(...,gt=0),db:Session = Depends(get_db),request = TaskUpdateSchema):
+    task_obj = db.query(TaskModel).filter_by(id = task_id).first()
+    if not task_obj:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    for field, value in request.model_dump(exclude_unset=True).items():
+        setattr(task_obj,field,value)
+    db.commit()
+    db.refresh(task_obj)
+
+    return task_obj
+
+@router.delete("/tasks/{task_id}",status_code=204)
+async def delete_tasks(task_id: int = Path(...,gt=0),db:Session = Depends(get_db)):
+    task_obj = db.query(TaskModel).filter_by(id = task_id).first()
+    if not task_obj:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db.delete(task_obj)
+    db.commit()
---
+++ core/tasks/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel,Field
+from typing import Optional
+from datetime import datetime
+
+
+
+class TaskBaseSchema(BaseModel):
+    title : str = Field(...,max_length=200,min_length=10,description="Title of the tasks")
+    description : Optional[str] = Field(None,max_length=500,description="Description of the tasks")
+    is_compleat : bool = Field(...,description="State of the tasks")
+
+class TaskCreateSchema(TaskBaseSchema):
+    pass
+
+
+class TaskUpdateSchema(TaskBaseSchema):
+    pass
+
+
+class TaskResponseSchema(TaskBaseSchema):
+    id : int = Field(...,description="Unique identifier of the object")
+
+    create_at : datetime = Field(...,description="Create date and time of the object")
+    update_at : datetime = Field(...,description="Update date and time of the object")
